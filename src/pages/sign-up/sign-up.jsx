523afa3133c0a65469b9d5460e5c1d79dda31bfd
lucy2n <lyusianaumenko@yandex.by>
2024-09-13 12:47:31 +0300

    feat: add transaction in sign-up

--- src/pages/sign-up/sign-up.jsx
+++ src/pages/sign-up/sign-up.jsx
@@ -5,56 +5,69 @@ import { registerUser } from '../../services/auth-servise';
 import { handlRegisterErrors } from '../../utils/utils';
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [username, setUsername] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await registerUser(email, password, username);
+      navigate('/login');
+    } catch (error) {
+      setError(handlRegisterErrors(error));
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await registerUser(email, password, username);
-            alert('User registered successfully');
-            navigate('/login');
-        } catch (error) {
-            setError(handlRegisterErrors(error));
-        }
-    };
+  return (
+    <div className="container p-5 my-5 border w-50">
+      <h2 className="text-primary mb-4">Sign Up</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Enter your name" 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+        </Form.Group>
+        
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control 
+            type="email" 
+            placeholder="Enter your email" 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <Form.Text className="text-danger">
+            {error && <span>{error}</span>}
+          </Form.Text>
+        </Form.Group>
 
-    return (
-        <div className="container p-5 my-5 border w-50">
-            <h2 className="text-primary mb-4">Sign Up</h2>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" onChange={(e) => setUsername(e.target.value)}/>
-            </Form.Group>
-                
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)}/>
-                <Form.Text className="text-danger">{error}</Form.Text>
-            </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password" 
+            placeholder="Enter your password" 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}/>
-            </Form.Group>
+        <button id="register-button" type="submit">
+          Submit
+        </button>
+      </Form>
 
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-                Submit
-            </Button>
-            </Form>
-            <div className='d-flex flex-column justify-content-center align-items-center'>
-                <p>Already have an account?</p>
-                <Button className="btn-link" type="button" onClick={() => navigate('/login')}>
-                    Sing In
-                </Button>
-            </div>
-        </div>
-    )
-}
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <p>Already have an account?</p>
+        <Button className="btn-link" type="button" onClick={() => navigate('/login')}>
+          Sign In
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
--- src/services/auth-servise.js
+++ src/services/auth-servise.js
@@ -1,22 +1,41 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { ref, set, update, get } from "firebase/database";
+import { ref, set, update, get, runTransaction } from "firebase/database";
 import { auth, db } from './firebase';
 
 export const registerUser = async (email, password, username) => {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-  
-    await set(ref(db, 'users/' + user.uid), {
-      username,
-      email: user.email,
-      registration_date: new Date().toISOString(),
-      last_login: new Date().toISOString(),
-      status: 'active',
-    });
-  
-    return user;
-  };
-  
+  const emailKey = email.replace('.', ',');
+
+  const emailRef = ref(db, `emails/${emailKey}`);
+
+  return runTransaction(emailRef, (currentValue) => {
+      if (currentValue === null) {
+          return true;
+      } else {
+          return;
+      }
+  }).then(async (transactionResult) => {
+      if (transactionResult.committed) {
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          const user = userCredential.user;
+
+          await set(ref(db, 'users/' + user.uid), {
+              id: user.uid,
+              username,
+              email: user.email,
+              registration_date: new Date().toISOString(),
+              last_login: new Date().toISOString(),
+              status: 'active',
+          });
+
+          return user;
+      } else {
+          throw new Error('Email is already in use.');
+      }
+  }).catch((error) => {
+      throw error;
+  });
+};
+
 export const loginUser = async (email, password) => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
